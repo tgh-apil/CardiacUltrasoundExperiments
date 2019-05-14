9c687b4b5ac045b280dabf7b869755782394b10a
Sachin Khargie <sachx1281@gmail.com>
2019-05-14 13:31:36 -0400

    Add files via upload

---
+++ GeneralScripts/camControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camControl : MonoBehaviour {
+
+    public float speed;
+    public float zoomSpeed;
+
+    public float rotHorizon;
+    public float rotVertical;
+
+    float zoom;
+
+    public bool xTEEOn;
+
+    public Camera[] mainCam;
+
+    void Update()
+    {
+        if (!xTEEOn)
+        {
+          viewController();
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+
+    void viewController()
+    {
+        rotHorizon = Input.GetAxis("Mouse X") * speed;
+        rotVertical = Input.GetAxis("Mouse Y") * speed;
+
+        for(int i = 0; i< mainCam.Length; i++)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                if (mainCam[0].name == "mainCamTTE")
+                {
+                    transform.Rotate(Vector3.forward, rotHorizon, Space.World);
+                    transform.Rotate(Vector3.right, -rotVertical, Space.Self);
+                }
+                else if (mainCam[0].name == "mainCamTEE")
+                {
+                    transform.Rotate(Vector3.up, rotHorizon, Space.World);
+                    transform.Rotate(Vector3.forward, -rotVertical, Space.Self);
+                }
+                else
+                {
+                    transform.Rotate(Vector3.up, rotHorizon, Space.World);
+                    transform.Rotate(Vector3.right, -rotVertical, Space.Self);
+                }
+            }
+
+            if (Input.GetMouseButton(2))
+            {
+                mainCam[i].transform.Translate(new Vector3(-rotHorizon * 5, -rotVertical * 5, 0), Space.Self);
+            }
+
+            zoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+            zoom = Mathf.Clamp(zoom, -45, 20);
+            mainCam[i].fieldOfView = 80 + zoom;
+        }
+    }
+}
---
+++ GeneralScripts/textureOffsetCycler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textureOffsetCycler : MonoBehaviour {
+
+    public float cyclerX = 0.015f;
+    public float cyclerY = 0.085f;
+    public Material mat;
+    Renderer rend;
+
+	void Update ()
+    {
+        //script modified from official Unity scripting page for mainTextureScale
+        float cycleX = Mathf.Sin(Time.time) * cyclerX + 1;
+        float cycleY = Mathf.Cos(Time.time) * cyclerY + 1;
+
+        mat.mainTextureScale = new Vector2(cycleX, cycleY);
+    }
+}
